2bcea4448910fce954537c8bd186189f25cdda22
chintyadewi <chintyadewi437@gmail.com>
2018-05-28 23:47:57 +0700

    use express.json and mysql

---
+++ server.js
@@ -0,0 +1,40 @@
+const express=require('express')
+const app=express()
+const mysql=require('mysql')
+const cors= require('cors')
+require('dotenv').config()
+const biodataRouter = require('./routes/biodata')
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+})
+
+con.connect(function (err) {
+  if (err) {
+    console.log('Database connection error')
+  } else {
+    console.log('Database connection success')
+  }
+})
+
+app.use(function(req, res, next){
+	req.con=con
+	next()
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/',function(req,res){
+  res.send('Welcome...')
+})
+
+app.use('/biodata', biodataRouter)
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000')
+})
