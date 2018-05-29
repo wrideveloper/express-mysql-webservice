023acd6f2fd5b65f30a88ba7d0e177d6a247a6ee
chintyadewi <chintyadewi437@gmail.com>
2018-05-29 23:45:20 +0700

    use express.json and mysql

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use('/',function(req,res){
+app.get('/',function(req,res){
   res.send('Welcome...')
 })
 
